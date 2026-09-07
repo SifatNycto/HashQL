b3ee89f1b991756842315232de8438c18f46f9b6
SifatNycto <sftmhd@gmail.com>
2026-09-08 02:41:51 +0600

    feat: validate required shcema

--- main.cpp
+++ main.cpp
@@ -34,26 +34,38 @@ int main()
         {
             std::string schema;
             std::getline(ss, schema);
+
+
             schema.erase(0, schema.find_first_not_of(" \t"));
 
-            schema.erase(0, 1);
-            schema.pop_back();
-
-            std::stringstream schemaStream(schema);
-            std::string column;
-
-            while (std::getline(schemaStream, column, ','))
+            if (schema.empty())
             {
-                column.erase(0, column.find_first_not_of(" \t"));
-                
-                std::stringstream columnStream(column);
-
-                std::string columnName;
-                std::string columnType;
-
-                columnStream >> columnName;
-                columnStream >> columnType;
+                std::cout << "\nError: Schema is required here.";
             }
+            else
+            {
+                schema.erase(0, 1);
+                schema.pop_back();
+
+
+                 std::stringstream schemaStream(schema);
+                std::string column;
+
+                while (std::getline(schemaStream, column, ','))
+                {
+                    column.erase(0, column.find_first_not_of(" \t"));
+                    
+                    std::stringstream columnStream(column);
+
+                    std::string columnName;
+                    std::string columnType;
+
+                    columnStream >> columnName;
+                    columnStream >> columnType;
+                }
+            }
+
+           
 
 
             if (argument.empty())
